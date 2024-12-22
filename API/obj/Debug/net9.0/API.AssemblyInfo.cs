dae267ed29fe57f10bb672ea97794173820d05c1
Stefan Tsenev <stefan.tsenev.edu@gmail.com>
2024-12-23 01:34:01 +0200

    Implement Register Endpoint

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5329654c31783b356babe8cea69b5ac6376327d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8757d12aa2426def8451ffadaea9f190460e77a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
